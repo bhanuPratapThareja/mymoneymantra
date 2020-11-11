52944e044177d3a4473a8274eca844b3d2cac9d6
pawankr123 <pawan.kumar1@qualtechedge.com>
2020-11-11 17:23:07 +0530

    key added in learnmore

--- components/LearnMore.js
+++ components/LearnMore.js
@@ -54,7 +54,7 @@ const LearnMore = props => {
 
                {sectionData.map((secData,index )=> {
                   return (
-                     <div className="learn-more-wrapper-content">
+                     <div className="learn-more-wrapper-content" key={secData.id}>
                         <div className="question" id={`ques-${index}`}>
                            <h3>{secData.heading}</h3>
                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
