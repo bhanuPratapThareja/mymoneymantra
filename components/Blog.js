d32eb7763260e228f6ad1106cdac32addccf41d8
pawankr123 <pawan.kumar1@qualtechedge.com>
2020-11-11 11:29:33 +0530

    blogs done css changes

--- components/Blog.js
+++ components/Blog.js
@@ -9,34 +9,53 @@ const Blog = props => {
       <section data-aos="fade-up" className="container blog-container aos-init aos-animate">
          <div className="blog">
             <h2>{blogHeading}</h2>
-            {blogsData.map(blogData => {
-               return (
-                  <div className="blog-wrapper" id="slider_blogs" key={blogData.id}>
-                     <div className="blog-wrapper-card card-1" id="blog-card-1">
-                        <div className="image_1">
-                        <img src={`${strapi.baseUrl}${blogData.image.url}`} alt={blogData.image.name} />
-                        </div>
-
-                        <div className="content">
-                           <h3>{blogData.heading}</h3>
-                           <h6>{blogData.sub_text}
-                        </h6>
-                           <div className="details">
-                              <span>{blogData.read_at}</span>
-                              <button>Read more</button>
-                           </div>
-                        </div>
-                     </div>
-                    
-                
+            <div className="blog-wrapper" id="slider_blogs">
+               <div className="blog-wrapper-card card-1" id="blog-card-1">
+                  <div className="image_1">
+                     <img src={`${strapi.baseUrl}${blogsData[0].image.url}`} alt={blogsData[0].image.name} />
                   </div>
-               )
-               }
-               )
+                  <div className="content">
+                     <h3>{blogsData[0].heading}</h3>
+                     <h6>{blogsData[0].sub_text}</h6>
+                     <div className="details">
+                        <span>{blogsData[0].read_at}</span>
+                        <button>Read more</button>
+                     </div>
+                  </div>
+               </div>
 
-            }
-        
-      </div>
+               <div className="blog-wrapper-card card-2" id="blog-card-2">
+                  <div className="image_2">
+                     <img src={`${strapi.baseUrl}${blogsData[1].image.url}`} alt={blogsData[1].image.name} />
+                  </div>
+                  <div className="content">
+                     <h3>{blogsData[1].heading}</h3>
+                     <h6>{blogsData[1].sub_text}</h6>
+                     <div className="details">
+                        <span>{blogsData[1].read_at}</span>
+                        <button>Read more</button>
+                     </div>
+                  </div>
+               </div>
+
+               <div className="blog-wrapper-card card-3" id="blog-card-3">
+                  <div className="image_3">
+                     <img src={`${strapi.baseUrl}${blogsData[2].image.url}`} alt={blogsData[2].image.name} />
+                  </div>
+                  <div className="content">
+                     <h3>{blogsData[2].heading}</h3>
+                     <h6>{blogsData[2].sub_text}</h6>
+                     <div className="details">
+                        <span>{blogsData[2].read_at}</span>
+                        <button>Read more</button>
+                     </div>
+                  </div>
+               </div>
+
+            </div>
+
+
+         </div>
       </section>
    )
 }
