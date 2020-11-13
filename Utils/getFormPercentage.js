cd0d1190f37d7546930cc3773581fe545e6565d0
BHANU THAREJA <bhanuthareja@BHANUs-MacBook-Pro.local>
2020-11-13 16:46:23 +0530

    170px from 210px

--- Utils/getFormPercentage.js
+++ Utils/getFormPercentage.js
@@ -1,15 +1,8 @@
 export const getFormCompletePercent = state => {
     let valueslength = 0
     const totalValues = state.totalValues
-    console.log('state inside percentagecomp',state);
-    delete state.percentageComplete;
+    delete state.percentageComplete
     delete state.totalValues;
-
-    console.log('---', (Object.values(state)));
-
-    (Object.values(state)).forEach(value => { 
-        if (value) valueslength++ })
-        
-
+    (Object.values(state)).forEach(value => { if (value) valueslength++ })
     return Math.ceil(valueslength * 100 / totalValues)
 }
