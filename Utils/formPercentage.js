af3b1ddb8d0bc89d775052ac73cc3fc9f465f1c2
BHANU THAREJA <bhanuthareja@BHANUs-MacBook-Pro.local>
2020-11-20 22:01:56 +0530

    percentage fixed order of inputs pending

--- Utils/formPercentage.js
+++ Utils/formPercentage.js
@@ -2,7 +2,17 @@ export const getFormPercentage = state => {
     let valueslength = 0
     const totalValues = state.totalValues
     deleteStateKeys(state);
-    (Object.values(state)).forEach(value => { if (value) valueslength++ })
+    console.log('state: ', state);
+    if(!Object.keys(state).length) {
+        return 0
+    };
+
+    (Object.values(state)).forEach(value => { 
+        console.log(value)
+        if (value) valueslength++ 
+    })
+
+    console.log('valueslength: ', valueslength)
     return Math.ceil(valueslength * 100 / totalValues)
 }
 
@@ -16,12 +26,29 @@ function deleteStateKeys(state) {
     if (!state.mothersFirstName || !state.mothersLastName) {
         delete state.mothersFullName
     }
+    if(!state.address1 || !state.address2 || !state.city || !state.pincode) {
+        delete state.address
+    }
+    if(!state.officeAddress1 || !state.officeAddress2 || !state.officeCity || !state.officePincode) {
+        delete state.officeAddress
+    }
+
     delete state.firstName
     delete state.lastName
     delete state.fathersFirstName
     delete state.fathersLastName
     delete state.mothersFirstName
     delete state.mothersLastName
+    delete state.address1
+    delete state.address2
+    delete state.nearBy
+    delete state.city
+    delete state.pincode
+    delete state.officeAddress1
+    delete state.officeAddress2
+    delete state.officeNearBy
+    delete state.officeCity
+    delete state.officePincode
     delete state.percentageComplete
     delete state.totalValues
 }
