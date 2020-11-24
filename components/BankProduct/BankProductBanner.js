07685182b4fef24de53ad3378c128ace8b58ced4
BHANU THAREJA <bhanuthareja@BHANUs-MacBook-Pro.local>
2020-11-24 13:16:40 +0530

    banners

--- components/BankProduct/BankProductBanner.js
+++ components/BankProduct/BankProductBanner.js
@@ -1,20 +1,20 @@
 import Strapi from '../../providers/strapi';
 
-const BankProductBanner= props =>{
+const BankProductBanner = props => {
     const strapi = new Strapi()
-    const {button,heading,image,sub_text}= props.data;
-    return(
+    const { button, heading, image, sub_text } = props.data;
+    return (
         <div className="credit-card-flow">
-        <section className="banner container">
-            <div className="banner-wrapper">
-    <h1>{heading}</h1>
-                <p>{sub_text}</p>
-    <button>{button}</button>
-            </div>
-            <div>
-                <img className="banner-card" src={`${strapi.baseUrl}${image.url}`} alt={image.name} />             
-            </div> 
-        </section>
+            <section className="banner container">
+                <div className="banner-wrapper">
+                    <h1>{heading}</h1>
+                    <p>{sub_text}</p>
+                    <button>{button}</button>
+                </div>
+                <div>
+                    <img className="banner-card" src={`${strapi.baseUrl}${image.url}`} alt={image.name} />
+                </div>
+            </section>
         </div>
     )
 }
