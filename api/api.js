55f9b7210a5833a906bf0b96e8f4ebfc05fe8a43
BHANU THAREJA <bhanuthareja@BHANUs-MacBook-Pro.local>
2020-12-06 18:59:41 +0530

    apis just with cities

--- api/api.js
+++ api/api.js
@@ -11,7 +11,10 @@ const api = {
             prodUrl: 'api/customer/v1/profile/',
             body: { request: { header: { correlationId: "25478965874", appId: "MMMWEBAPP" }, payload: { mobileNo: "9999000090", customerId: "9999000090" } } }
         },
-        bank: {
+        banks: {
+
+        },
+        companies: {
 
         },
         cities: {
