e9b8dec071f8528c4cabf7b0be0636be3c50d634
BHANU THAREJA <bhanuthareja@BHANUs-MacBook-Pro.local>
2020-12-31 05:46:29 +0530

    view detail button fixed

--- components/common/DetailsBanner.js
+++ components/common/DetailsBanner.js
@@ -20,7 +20,7 @@ const DetailsBanner = props => {
 
     const { bank_name } = props.bank
     console.log(props.product)
-    const { product_name, product_image } = props.product
+    const { product_name, product_image, product_card_name } = props.product
     const details = props.data.credit_cards_details_banner || props.data.personal_loans_details_banner
 
     const onButtonClick = (buttonText, bank, offer) => {
@@ -59,7 +59,7 @@ const DetailsBanner = props => {
         <div className="combined-wrapper">
             <section className="banner container">
                 <div className="banner-wrapper">
-                    <h1><b>{bank_name}</b><br />{product_name}</h1>
+                    <h1><b>{bank_name}</b><br />{product_name || product_card_name}</h1>
                     <div dangerouslySetInnerHTML={{ __html: details.content }}></div>
                     {productDetails ? <span className="details-button-div">
                         <button onClick={() => onButtonClick(productDetails.productDecision, props.bank, productDetails)}>{productDetails.productDecision}</button>
