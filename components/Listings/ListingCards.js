07db3ca16a0f270a6fa97083d58fc60ce27da34c
BHANU THAREJA <bhanuthareja@BHANUs-MacBook-Pro.local>
2021-01-15 17:52:02 +0530

    submitting for css forms

--- components/Listings/ListingCards.js
+++ components/Listings/ListingCards.js
@@ -25,12 +25,11 @@ const ListingCards = props => {
         const query = { bankName }
 
         switch (buttonText) {
-            // case 'Apply Now':
+            case 'Apply Now':
             case 'Instant Approval':
                 pathname = `/${primaryPath}/thank-you`
                 break
 
-                case 'Apply Now':
             case 'EConnect':
                 pathname = `/${primaryPath}/long-form/${bankSlug}/${productSlug}`
                 break
