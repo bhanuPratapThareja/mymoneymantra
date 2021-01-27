e5e35d3ff69c59c08a6213cce83fc292959c358e
Manish Raj <manish.raj@qualtechedge.com>
2021-01-27 17:46:00 +0530

    reading-time-plugin-integrated

--- components/common/BlogsDetails.js
+++ components/common/BlogsDetails.js
@@ -1,9 +1,12 @@
 import Image from '../ImageComponent/ImageComponent'
 import CommentSection from './CommentSection'
 
+
 const BlogsDetails = props => {
     // const { header, read_text, blog_sub_category, content, display_short_text } = props.data.blogger
     const { header, read_text, blog_sub_category, content, display_short_text } = props.data
+    const readingTime = require('reading-time');
+    const blogreadTime = readingTime(content);
     const subCategory = blog_sub_category ? blog_sub_category.blog_sub_category : ""
     console.log(subCategory)
     const { blogId, commentData } = props
@@ -13,7 +16,7 @@ const BlogsDetails = props => {
                 <div className="blog-wrapper-content">
                     <div className="blog-wrap-top">
                         <h1 dangerouslySetInnerHTML={{ __html: header }}></h1>
-                        <span>{read_text}//{subCategory}</span>
+                        <span>{blogreadTime.text}//{subCategory}</span>
                         <span dangerouslySetInnerHTML={{ __html: content }}></span>
                     </div>
                         <CommentSection blogId={blogId} commentData={commentData} />
