2cc937e213178db2738df483e128ee6347586171
VikasTiwari26 <imtiwarivikas26@gmail.com>
2021-01-28 19:20:51 +0530

    filters and get api config changed

--- components/common/Blogger.js
+++ components/common/Blogger.js
@@ -5,7 +5,9 @@ import Image from '../ImageComponent/ImageComponent'
 const Blogger = props => {
     const router = useRouter()
     let { section_heading, bloggers } = props.data
-
+    let popularBlogs = bloggers.filter(blog => blog.popular === true)
+    console.log(popularBlogs)
+    console.log(bloggers)
     const onOpenBlog = blog => {
         router.push({ pathname: '/blog/details', query: { id: blog.id } })
     }
@@ -15,7 +17,7 @@ const Blogger = props => {
             <div className="blog">
                 <div dangerouslySetInnerHTML={{ __html: section_heading }}></div>
                 <div className="blog-wrapper" id="slider_blogs">
-                    {bloggers.map((blog, i) => {
+                    {popularBlogs.map((blog, i) => {
                         const { header, short_text, image, read_text, redirect_url, id, createdAt, popular } = blog
                         const date = new Date(createdAt);
                         const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
@@ -24,15 +26,13 @@ const Blogger = props => {
                         const createdDate = `${da} ${mo} ${ye}`;
                         const blogClasses = ['blog-wrapper-card', `card-${i + 1}`]
                         return (
-                            popular ? <div className={blogClasses.join(' ')} id={`blog-card-${i + 1}`} key={id}>
+                            (i < 3) ? <div className={blogClasses.join(' ')} id={`blog-card-${i + 1}`} key={id}>
                                 <div className={`image_${i + 1}`}>
                                     <Image image={image} />
                                 </div>
                                 <div className="content">
                                     <span dangerouslySetInnerHTML={{ __html: header }}></span>
                                     <span dangerouslySetInnerHTML={{ __html: short_text }}></span>
-                                    
-
                                     <div className="details">
                                         <span>{createdDate} </span><span>{read_text}</span>
                                         <button onClick={() => onOpenBlog(blog)}>Read more</button>
