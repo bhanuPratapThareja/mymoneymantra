6ec1df58b2f738f39691d9aaaa2acf93f6384f8c
ChandrakantPal <sanjay161097@gmail.com>
2021-02-03 12:54:45 +0530

    credit score page

---
+++ pages/credit-score/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import CreditScore from '../../components/common/CreditScore'
+import Layout from '../../components/Layout'
+
+const creditScoreProfile = (props) => {
+  return <Layout>{/* <CreditScore /> */}</Layout>
+}
+
+export default creditScoreProfile
