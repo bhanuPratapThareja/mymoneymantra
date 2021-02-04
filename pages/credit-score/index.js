cd6e6940ea6823a92fb115284c5af657d493686f
ChandrakantPal <sanjay161097@gmail.com>
2021-02-04 16:06:07 +0530

    credit-score-profile page

--- pages/credit-score/index.js
+++ pages/credit-score/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
-import CreditScore from '../../components/common/CreditScore'
+import AccountSummary from '../../components/CreditScore/AccountSummary'
+import CreditOverview from '../../components/CreditScore/CreditOverview'
+import CreditScoreBanner from '../../components/CreditScore/CreditScoreBanner'
+import FactorsAffecting from '../../components/CreditScore/FactorsAffecting'
+import OffersForYou from '../../components/CreditScore/OffersForYou'
+import TipSection from '../../components/CreditScore/TipSection'
 import Layout from '../../components/Layout'
+import { getClassesForPage } from '../../utils/classesForPage'
 
 const creditScoreProfile = (props) => {
-  return <Layout>{/* <CreditScore /> */}</Layout>
+  return (
+    <div className={props.pageClasses}>
+      <Layout>
+        <CreditScoreBanner />
+        <TipSection />
+        <FactorsAffecting />
+        <CreditOverview />
+        <AccountSummary />
+        <OffersForYou />
+      </Layout>
+    </div>
+  )
 }
-
+export async function getServerSideProps(ctx) {
+  const primaryPath = 'credit-score-profile'
+  const pageClasses = getClassesForPage(primaryPath)
+  return { props: { pageClasses } }
+}
 export default creditScoreProfile
