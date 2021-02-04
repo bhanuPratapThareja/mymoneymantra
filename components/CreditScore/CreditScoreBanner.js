8614136ea433c5871bf25c3caf5ecc36daf0c011
ChandrakantPal <sanjay161097@gmail.com>
2021-02-04 16:05:19 +0530

    section components for credit-profile

---
+++ components/CreditScore/CreditScoreBanner.js
@@ -0,0 +1,28 @@
+const CreditScoreBanner = () => {
+  return (
+    <section data-aos="fade-up" className="container banner new-credit-wrap">
+      <div className="creditScore-wrapper">
+        <div className="creditScore-wrapper-left">
+          <div>
+            <h2>Your Credit Score</h2>
+            <img className="underline-img" src="https://the1thing.github.io/MyMoneyMantra/build/images/CP_profile/underline.png" alt="" />
+          </div>
+          <div className="score-scale-img">
+            <img src="https://the1thing.github.io/MyMoneyMantra/build/images/CP_profile/score-scale.png" alt="" />
+          </div>
+        </div>
+        <div className="creditScore-wrapper-right">
+          <div>
+            <h2>Your Credit Score History</h2>
+            <img className="underline-img" src="https://the1thing.github.io/MyMoneyMantra/build/images/CP_profile/underline.png" alt="" />
+          </div>
+          <div className="score-graph-img">
+            <h2>Graph</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CreditScoreBanner
