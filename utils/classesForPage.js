6193fea36cc4c22e27b1fddf51ca7bd775d016b2
ChandrakantPal <sanjay161097@gmail.com>
2021-02-04 16:04:34 +0530

    css for credit-score-profile

--- utils/classesForPage.js
+++ utils/classesForPage.js
@@ -1,42 +1,42 @@
 const classes = {
-    'home-page': ['credit-card-flow', 'homepage-flow'],
-    'credit-cards': ['credit-card-flow'],
-    'personal-loans': ['credit-card-flow', 'personal-loan-flow'],
-    'home-loans': ['credit-card-flow', 'personal-loan-flow', 'home-loan-flow', 'gold-loan-flow', 'health-flow'],
-    'blog': ['credit-card-flow', 'b2c-flow', 'blogs-flow'],
-    'Forms': ['credit-card-flow', 'homepage-flow'],
-    'long-form': ['long-form'],
-    'listings': {
-        'credit-cards': ['listings'],
-        'personal-loans': ['listings', 'personal-loan-listing'],
-        'home-loans': ['listings', 'personal-loan-listing', 'home-loan-listing']
-    },
-    'thank-you': {
-        'credit-cards': ['credit-card-flow', 'thankyou-page'],
-        'personal-loans': ['credit-card-flow', 'thankyou-page', 'personal-loan-thankyou'],
-        'home-loans': ['credit-card-flow', 'thankyou-page', 'personal-loan-thankyou', 'home-loan-thank-you', 'health-flow'],
-        'rkpl': ['credit-card-flow', 'thankyou-page']
-    },
-    'details': {
-        'credit-cards': ['credit-card-flow', 'c-detail-page'],
-        'personal-loans': ['credit-card-flow', 'c-detail-page', 'personal-detail-flow'],
-        'home-loans': ['credit-card-flow', 'c-detail-page', 'personal-detail-flow'],
-        'blog': ['credit-card-flow', 'b2c-flow', 'blogs-flow', 'blog-details']
-    }
-
+  'home-page': ['credit-card-flow', 'homepage-flow'],
+  'credit-cards': ['credit-card-flow'],
+  'personal-loans': ['credit-card-flow', 'personal-loan-flow'],
+  'home-loans': ['credit-card-flow', 'personal-loan-flow', 'home-loan-flow', 'gold-loan-flow', 'health-flow'],
+  blog: ['credit-card-flow', 'b2c-flow', 'blogs-flow'],
+  Forms: ['credit-card-flow', 'homepage-flow'],
+  'long-form': ['long-form'],
+  listings: {
+    'credit-cards': ['listings'],
+    'personal-loans': ['listings', 'personal-loan-listing'],
+    'home-loans': ['listings', 'personal-loan-listing', 'home-loan-listing'],
+  },
+  'thank-you': {
+    'credit-cards': ['credit-card-flow', 'thankyou-page'],
+    'personal-loans': ['credit-card-flow', 'thankyou-page', 'personal-loan-thankyou'],
+    'home-loans': ['credit-card-flow', 'thankyou-page', 'personal-loan-thankyou', 'home-loan-thank-you', 'health-flow'],
+    rkpl: ['credit-card-flow', 'thankyou-page'],
+  },
+  details: {
+    'credit-cards': ['credit-card-flow', 'c-detail-page'],
+    'personal-loans': ['credit-card-flow', 'c-detail-page', 'personal-detail-flow'],
+    'home-loans': ['credit-card-flow', 'c-detail-page', 'personal-detail-flow'],
+    blog: ['credit-card-flow', 'b2c-flow', 'blogs-flow', 'blog-details'],
+  },
+  'credit-score-profile': ['credit-card-flow', 'credProfile', 'dashboard', 'app-progress', 'app-status', 'creditProfile-score-flow'],
 }
 
 export const getClassesForPage = (primary, secondary) => {
-    let pageClasses = ''
-    if (!secondary) {
-        pageClasses = classes[primary]
-    } else {
-        pageClasses = classes[secondary][primary]
-    }
+  let pageClasses = ''
+  if (!secondary) {
+    pageClasses = classes[primary]
+  } else {
+    pageClasses = classes[secondary][primary]
+  }
 
-    if (pageClasses && pageClasses.length) {
-        return pageClasses.join(' ')
-    } else {
-        return ''
-    }
+  if (pageClasses && pageClasses.length) {
+    return pageClasses.join(' ')
+  } else {
+    return ''
+  }
 }
