1189a93911e243ecf54b5565428f2ac3d88bfe1f
BHANU THAREJA <bhanuthareja@BHANUs-MacBook-Pro.local>
2021-02-05 11:20:10 +0530

    upload button verified handle

--- utils/formHandle.js
+++ utils/formHandle.js
@@ -306,8 +306,7 @@ export const updateInputsValidity = (inputs, field, errorMsgs) => {
             errors = true
           } else {
             inp.error = false;
-            inp.errorMsg = "";
-            inp.verified = true;
+            inp.errorMsg = "";     
           }
 
 
