1a4746b08873efbbfaab728002f2a0ff6f90b8e6
ChandrakantPal <sanjay161097@gmail.com>
2021-02-06 17:39:58 +0530

    application

---
+++ components/UserProfile/Application.js
@@ -0,0 +1,46 @@
+const Application = () => {
+  return (
+    <div className="applications-cards-wrapper">
+      <div className="popular-cards-slider-card">
+        <div className="popular-cards-slider-card-top">
+          <div className="head">
+            <h3>
+              <b className="card_name">RBL Bank</b>
+              <br />
+              Platinum Delight Credit Card
+            </h3>
+            <img src="build/images/icons/citi-logo.png" />
+          </div>
+          <h2>Lifetime win points</h2>
+          <h5>Application No#1234567890</h5>
+        </div>
+        <div className="popular-cards-slider-card-bottom">
+          <div>
+            <h5>Application Status: Successful</h5>
+          </div>
+        </div>
+      </div>
+      <div className="popular-cards-slider-card">
+        <div className="popular-cards-slider-card-top">
+          <div className="head">
+            <h3>
+              <b className="card_name">RBL Bank</b>
+              <br />
+              Platinum Delight Credit Card
+            </h3>
+            <img src="build/images/icons/citi-logo.png" />
+          </div>
+          <h2>Lifetime win points</h2>
+          <h5>Application No#1234567890</h5>
+        </div>
+        <div className="popular-cards-slider-card-bottom">
+          <div>
+            <h5>Application Status: Successful</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Application
