c50203a2feee7a2c75a24f12ec22efee99ad2c4c
ChandrakantPal <sanjay161097@gmail.com>
2021-02-06 17:40:24 +0530

    offers

---
+++ components/UserProfile/Offers.js
@@ -0,0 +1,54 @@
+const Offers = () => {
+  return (
+    <div className="offers-cards-wrapper">
+      <div className="popular-cards-slider-card">
+        <div className="popular-cards-slider-card-top">
+          <div className="head">
+            <h3>
+              <b className="card_name">RBL Bank</b>
+              <br />
+              Platinum Delight Credit Card
+            </h3>
+            <img src="build/images/icons/citi-logo.png" />
+          </div>
+          <div className="content">
+            <ul>
+              <li>Earn 10 reward points for every ₹125 spent at apparel &amp; department stores</li>
+              <li>Instant Redemption at select partner stores</li>
+            </ul>
+          </div>
+        </div>
+        <div className="popular-cards-slider-card-bottom">
+          <div>
+            <h5>Lifetime reward points</h5>
+          </div>
+        </div>
+      </div>
+      <div className="popular-cards-slider-card">
+        <div className="popular-cards-slider-card-top">
+          <div className="head">
+            <h3>
+              <b className="card_name">RBL Bank</b>
+              <br />
+              Platinum Delight Credit Card
+            </h3>
+            <img src="build/images/icons/citi-logo.png" />
+          </div>
+          <div className="content">
+            <ul>
+              <li>Earn 10 reward points for every ₹125 spent at apparel &amp; department stores</li>
+              <li>Instant Redemption at select partner stores</li>
+            </ul>
+          </div>
+        </div>
+        <div className="popular-cards-slider-card-bottom">
+          <div>
+            <h5>Lifetime reward points</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Offers
