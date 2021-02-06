894c2d763ab4bf0d9af72931a5ae5d5931e13bac
ChandrakantPal <sanjay161097@gmail.com>
2021-02-06 17:40:43 +0530

    refer and earn

---
+++ components/UserProfile/ReferEarn.js
@@ -0,0 +1,50 @@
+const ReferEarn = () => {
+  return (
+    <div className="refer-cards-wrapper">
+      <div className="refer-card">
+        <h3>Reward Points</h3>
+        <ul>
+          <li>Earn 10 reward points for every ₹125 spent at apparel &amp; department stores</li>
+          <li>Instant Redemption at select partner stores</li>
+        </ul>
+        <div className="redeem">
+          <div className="points">
+            <span>Your Points</span>
+            <h5>3900</h5>
+          </div>
+          <button>Redeem Now</button>
+        </div>
+      </div>
+      <div className="refer-card">
+        <h3>Referral Code</h3>
+        <ul>
+          <li>Earn 10 reward points for every ₹125 spent at apparel &amp; department stores</li>
+          <li>Instant Redemption at select partner stores</li>
+        </ul>
+        <div className="redeem">
+          <div className="points">
+            <span>Your Code</span>
+            <h5>XU79D</h5>
+          </div>
+          <button>Refer Now</button>
+        </div>
+      </div>
+      <div className="refer-card">
+        <h3>Invite</h3>
+        <ul>
+          <li>Earn 10 reward points for every ₹125 spent at apparel &amp; department stores</li>
+          <li>Instant Redemption at select partner stores</li>
+        </ul>
+        <div className="redeem">
+          <div className="points">
+            <span>Your Points</span>
+            <h5>2300</h5>
+          </div>
+          <button>Invite Now</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ReferEarn
