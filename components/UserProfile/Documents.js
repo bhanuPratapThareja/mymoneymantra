cc5def3d6cffa30aaae6138abcbafa9e94596877
ChandrakantPal <sanjay161097@gmail.com>
2021-02-06 17:39:45 +0530

    documents

---
+++ components/UserProfile/Documents.js
@@ -0,0 +1,115 @@
+const Documents = () => {
+  return (
+    <div className="documents-wrapper">
+      <div className="docs-container">
+        <span>Identity Proof</span>
+        <div className="identity-options col-span">
+          <div className="options">
+            <h5>Aadhaar Card</h5>
+            <div>
+              <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 22a5.86 5.86 0 01-6-5.7V6.13A4.24 4.24 0 0110.33 2a4.24 4.24 0 014.34 4.13v10.18a2.67 2.67 0 01-5.33 0V6.92a1 1 0 012 0v9.39a.67.67 0 001.33 0V6.13A2.25 2.25 0 0010.33 4 2.25 2.25 0 008 6.13V16.3a3.86 3.86 0 004 3.7 3.86 3.86 0 004-3.7V6.13a1 1 0 012 0V16.3a5.86 5.86 0 01-6 5.7z"
+                  fill="#fff"
+                ></path>
+              </svg>
+              <h6>aadhaar.jpeg</h6>
+            </div>
+            <span>uploaded successfully</span>
+          </div>
+        </div>
+        <div className="identity-options">
+          <div className="options">
+            <h5>PAN Card</h5>
+            <span className="error">please upload a .jpeg/pdf file</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="docs-container">
+        <span>Income Proof</span>
+        <div className="identity-options col-span">
+          <div className="options">
+            <h5>Bank Statements</h5>
+            <div>
+              <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 22a5.86 5.86 0 01-6-5.7V6.13A4.24 4.24 0 0110.33 2a4.24 4.24 0 014.34 4.13v10.18a2.67 2.67 0 01-5.33 0V6.92a1 1 0 012 0v9.39a.67.67 0 001.33 0V6.13A2.25 2.25 0 0010.33 4 2.25 2.25 0 008 6.13V16.3a3.86 3.86 0 004 3.7 3.86 3.86 0 004-3.7V6.13a1 1 0 012 0V16.3a5.86 5.86 0 01-6 5.7z"
+                  fill="#fff"
+                ></path>
+              </svg>
+              <h6>apr19-mar20.pdf</h6>
+            </div>
+            <span>uploaded successfully</span>
+          </div>
+        </div>
+        <div className="identity-options">
+          <div className="options">
+            <h5>Salary Slips</h5>
+            <div>
+              <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 22a5.86 5.86 0 01-6-5.7V6.13A4.24 4.24 0 0110.33 2a4.24 4.24 0 014.34 4.13v10.18a2.67 2.67 0 01-5.33 0V6.92a1 1 0 012 0v9.39a.67.67 0 001.33 0V6.13A2.25 2.25 0 0010.33 4 2.25 2.25 0 008 6.13V16.3a3.86 3.86 0 004 3.7 3.86 3.86 0 004-3.7V6.13a1 1 0 012 0V16.3a5.86 5.86 0 01-6 5.7z"
+                  fill="#fff"
+                ></path>
+              </svg>
+              <h6>3monthslip.jpeg</h6>
+            </div>
+            <span>uploaded successfully</span>
+          </div>
+        </div>
+        <div className="identity-options">
+          <div className="options">
+            <h5>Form 16 / 26AS / ITR</h5>
+            <div>
+              <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 22a5.86 5.86 0 01-6-5.7V6.13A4.24 4.24 0 0110.33 2a4.24 4.24 0 014.34 4.13v10.18a2.67 2.67 0 01-5.33 0V6.92a1 1 0 012 0v9.39a.67.67 0 001.33 0V6.13A2.25 2.25 0 0010.33 4 2.25 2.25 0 008 6.13V16.3a3.86 3.86 0 004 3.7 3.86 3.86 0 004-3.7V6.13a1 1 0 012 0V16.3a5.86 5.86 0 01-6 5.7z"
+                  fill="#fff"
+                ></path>
+              </svg>
+              <h6>form16A.pdf</h6>
+            </div>
+            <span>uploaded successfully</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="docs-container">
+        <span>Address Proof</span>
+        <div className="identity-options col-span">
+          <div className="options">
+            <h5>Rent Agreement</h5>
+            <div>
+              <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 22a5.86 5.86 0 01-6-5.7V6.13A4.24 4.24 0 0110.33 2a4.24 4.24 0 014.34 4.13v10.18a2.67 2.67 0 01-5.33 0V6.92a1 1 0 012 0v9.39a.67.67 0 001.33 0V6.13A2.25 2.25 0 0010.33 4 2.25 2.25 0 008 6.13V16.3a3.86 3.86 0 004 3.7 3.86 3.86 0 004-3.7V6.13a1 1 0 012 0V16.3a5.86 5.86 0 01-6 5.7z"
+                  fill="#fff"
+                ></path>
+              </svg>
+              <h6>Rent.pdf</h6>
+            </div>
+            <span>uploaded successfully</span>
+          </div>
+        </div>
+        <div className="identity-options">
+          <div className="options">
+            <h5>Electricity / Water / Telephone / Utility Bill</h5>
+            <div>
+              <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12 22a5.86 5.86 0 01-6-5.7V6.13A4.24 4.24 0 0110.33 2a4.24 4.24 0 014.34 4.13v10.18a2.67 2.67 0 01-5.33 0V6.92a1 1 0 012 0v9.39a.67.67 0 001.33 0V6.13A2.25 2.25 0 0010.33 4 2.25 2.25 0 008 6.13V16.3a3.86 3.86 0 004 3.7 3.86 3.86 0 004-3.7V6.13a1 1 0 012 0V16.3a5.86 5.86 0 01-6 5.7z"
+                  fill="#fff"
+                ></path>
+              </svg>
+              <h6>Electricity.pdf</h6>
+            </div>
+            <span>uploaded successfully</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Documents
