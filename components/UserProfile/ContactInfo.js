94a555aa00f7d3a7cedd4e2edee8c2556a59e1c4
ChandrakantPal <sanjay161097@gmail.com>
2021-02-07 23:40:04 +0530

    form handling for personal info enabling editing for contact info

--- components/UserProfile/ContactInfo.js
+++ components/UserProfile/ContactInfo.js
@@ -4,46 +4,87 @@ const ContactInfo = () => {
   const [editing, setEditing] = useState(false)
 
   return (
-    <form className="contact-wrapper">
-      <div className="shortforms-container">
-        <div className="form__group field read-part">
-          <input readonly="" className="form__field" type="text" value="+91 8790765432" id="mob-num" placeholder="Mobile Number" required="" />
-          <label className="form__label" for="mob-num">
-            Mobile Number
-          </label>
-        </div>
-        <div className="form__group field read-part">
-          <input readonly="" className="form__field" type="text" value="venkat90@gmail.com" id="email" placeholder="Email ID" required="" />
-          <label className="form__label" for="email">
-            Email ID
-          </label>
-        </div>
-        <div className="form__group field read-part">
-          <input readonly="" className="form__field" type="text" value="A-99, Manish Marg, Nemi Nagar, Jaipur, Rajasthan-302021" id="current-address" placeholder="Current Address" required="" />
-          <label className="form__label" for="current-address">
-            Current Address
-          </label>
-        </div>
-        <div className="form__group field read-part">
-          <input readonly="" className="form__field" type="text" value="B-21, Rajouri Garden, New Delhi, Delhi-110021" id="permanent-address" placeholder="Permanent Address" required="" />
-          <label className="form__label" for="permanent-address">
-            Permanent Address
-          </label>
-        </div>
-      </div>
-      <div className="save-options">
-        <button type="button" className="save-contact" id="save-contact">
-          Save
-        </button>
-        <button type="button" className="cancel" id="cancel">
-          Cancel
-        </button>
-      </div>
+    <>
+      {editing ? (
+        <form className="contact-wrapper" style={{ display: 'block' }}>
+          <div className="shortforms-container">
+            <div className="form__group field">
+              <input className="form__field" type="text" value="+91 8790765432" id="mob-num" placeholder="Mobile Number" required="" />
+              <label className="form__label" for="mob-num">
+                Mobile Number
+              </label>
+            </div>
+            <div className="form__group field">
+              <input className="form__field" type="text" value="venkat90@gmail.com" id="email" placeholder="Email ID" required="" />
+              <label className="form__label" for="email">
+                Email ID
+              </label>
+            </div>
+            <div className="form__group field">
+              <input className="form__field" type="text" value="A-99, Manish Marg, Nemi Nagar, Jaipur, Rajasthan-302021" id="current-address" placeholder="Current Address" required="" />
+              <label className="form__label" for="current-address">
+                Current Address
+              </label>
+            </div>
+            <div className="form__group field">
+              <input className="form__field" type="text" value="B-21, Rajouri Garden, New Delhi, Delhi-110021" id="permanent-address" placeholder="Permanent Address" required="" />
+              <label className="form__label" for="permanent-address">
+                Permanent Address
+              </label>
+            </div>
+          </div>
+          <div className="save-options">
+            <button type="button" className="save-contact" id="save-contact" onClick={() => setEditing(false)}>
+              Save
+            </button>
+            <button type="button" className="cancel" id="cancel" onClick={() => setEditing(false)}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      ) : (
+        <form className="contact-wrapper">
+          <div className="shortforms-container">
+            <div className="form__group field read-part">
+              <input readonly className="form__field" type="text" value="+91 8790765432" id="mob-num" placeholder="Mobile Number" required="" />
+              <label className="form__label" for="mob-num">
+                Mobile Number
+              </label>
+            </div>
+            <div className="form__group field read-part">
+              <input readonly className="form__field" type="text" value="venkat90@gmail.com" id="email" placeholder="Email ID" required="" />
+              <label className="form__label" for="email">
+                Email ID
+              </label>
+            </div>
+            <div className="form__group field read-part">
+              <input readonly className="form__field" type="text" value="A-99, Manish Marg, Nemi Nagar, Jaipur, Rajasthan-302021" id="current-address" placeholder="Current Address" required="" />
+              <label className="form__label" for="current-address">
+                Current Address
+              </label>
+            </div>
+            <div className="form__group field read-part">
+              <input readonly className="form__field" type="text" value="B-21, Rajouri Garden, New Delhi, Delhi-110021" id="permanent-address" placeholder="Permanent Address" required="" />
+              <label className="form__label" for="permanent-address">
+                Permanent Address
+              </label>
+            </div>
+          </div>
+          <div className="save-options">
+            <button type="button" className="save-contact" id="save-contact" onClick={() => setEditing(false)}>
+              Save
+            </button>
+            <button type="button" className="cancel" id="cancel" onClick={() => setEditing(false)}>
+              Cancel
+            </button>
+          </div>
 
-      <button type="button" id="edit-contact" className="edit-button">
-        Edit
-      </button>
-    </form>
+          <button type="button" id="edit-contact" className="edit-button" onClick={() => setEditing(true)}>
+            Edit
+          </button>
+        </form>
+      )}
+    </>
   )
 }
 
--- components/UserProfile/PersonalInfo.js
+++ components/UserProfile/PersonalInfo.js
@@ -2,20 +2,31 @@ import { useState } from 'react'
 
 const PersonalInfo = () => {
   const [editing, setEditing] = useState(false)
+  const [firstName, setFirstName] = useState('Venkitaraman')
+  const [lastName, setLastName] = useState('Hariharan')
+  const [dob, setDob] = useState('')
+  const [gender, setGender] = useState('Male')
+  const [maritalStatus, setMaritalStatus] = useState('Single')
+  const [panNumber, setPanNumber] = useState('XXXX728KYZ1')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="personal-wrapper">
       {editing ? (
-        <form className="personal-forms-wrapper" style={{ display: 'block' }}>
+        <form className="personal-forms-wrapper" style={{ display: 'block' }} onSubmit={submitHandler}>
           <h5>Full Name</h5>
           <div className="shortforms-container personal-style">
             <div className="form__group field">
-              <input value="" className="form__field" type="text" id="mother-f-name" placeholder="First Name" required="" />
+              <input value={firstName} className="form__field" type="text" id="mother-f-name" placeholder="First Name" required="" onChange={(e) => setFirstName(e.target.value)} />
               <label className="form__label" for="mother-f-name">
                 First Name
               </label>
             </div>
             <div className="form__group field">
-              <input value="" className="form__field" type="text" id="mother-l-name" placeholder="Last Name" required="" />
+              <input value={lastName} className="form__field" type="text" id="mother-l-name" placeholder="Last Name" required="" onChange={(e) => setLastName(e.target.value)} />
               <label className="form__label" for="mother-l-name">
                 Last Name
               </label>
@@ -26,7 +37,7 @@ const PersonalInfo = () => {
             <div className="form__group field">
               <div role="wrapper" className="gj-datepicker gj-datepicker-md gj-unselectable">
                 <input
-                  value=""
+                  value={dob}
                   className="form__field profile-dob datepicker gj-textbox-md"
                   type="text"
                   id="dob"
@@ -36,6 +47,7 @@ const PersonalInfo = () => {
                   data-guid="2c92f534-a412-9063-7136-166bf9b6a4d8"
                   data-datepicker="true"
                   role="input"
+                  onChange={(e) => setDob(e.target.value)}
                 />
                 <i className="gj-icon" role="right-icon">
                   event
@@ -48,9 +60,9 @@ const PersonalInfo = () => {
           </div>
           <h5>Gender</h5>
           <div className="shortforms-container gender-style">
-            <input value="" className="lets-checkbox" type="radio" id="female" name="gender" required="" />
-            <input value="" className="lets-checkbox" type="radio" id="male" name="gender" required="" />
-            <input value="" className="lets-checkbox" type="radio" id="other" name="gender" required="" />
+            <input value="Female" className="lets-checkbox" type="radio" id="female" name="gender" required="" onChange={(e) => setGender(e.target.value)} checked={gender === 'Female'} />
+            <input value="Male" className="lets-checkbox" type="radio" id="male" name="gender" required="" onChange={(e) => setGender(e.target.value)} checked={gender === 'Male'} />
+            <input value="Other" className="lets-checkbox" type="radio" id="other" name="gender" required="" onChange={(e) => setGender(e.target.value)} checked={gender === 'Other'} />
 
             <label for="female">Female</label>
             <label for="male">Male</label>
@@ -58,11 +70,56 @@ const PersonalInfo = () => {
           </div>
           <h5>Marital Status</h5>
           <div className="shortforms-container marital-style">
-            <input value="" className="lets-checkbox" type="radio" id="single" name="Marital" required="" />
-            <input value="" className="lets-checkbox" type="radio" id="married" name="Marital" required="" />
-            <input value="" className="lets-checkbox" type="radio" id="separated" name="Marital" required="" />
-            <input value="" className="lets-checkbox" type="radio" id="divorced" name="Marital" required="" />
-            <input value="" className="lets-checkbox" type="radio" id="widowed" name="Marital" required="" />
+            <input
+              value="Single"
+              className="lets-checkbox"
+              type="radio"
+              id="single"
+              name="Marital"
+              required=""
+              onChange={(e) => setMaritalStatus(e.target.value)}
+              checked={maritalStatus === 'Single'}
+            />
+            <input
+              value="Married"
+              className="lets-checkbox"
+              type="radio"
+              id="married"
+              name="Marital"
+              required=""
+              onChange={(e) => setMaritalStatus(e.target.value)}
+              checked={maritalStatus === 'Married'}
+            />
+            <input
+              value="Separated"
+              className="lets-checkbox"
+              type="radio"
+              id="separated"
+              name="Marital"
+              required=""
+              onChange={(e) => setMaritalStatus(e.target.value)}
+              checked={maritalStatus === 'Separated'}
+            />
+            <input
+              value="Divorced"
+              className="lets-checkbox"
+              type="radio"
+              id="divorced"
+              name="Marital"
+              required=""
+              onChange={(e) => setMaritalStatus(e.target.value)}
+              checked={maritalStatus === 'Divorced'}
+            />
+            <input
+              value="Widowed"
+              className="lets-checkbox"
+              type="radio"
+              id="widowed"
+              name="Marital"
+              required=""
+              onChange={(e) => setMaritalStatus(e.target.value)}
+              checked={maritalStatus === 'Widowed'}
+            />
 
             <label for="single">Single</label>
             <label for="married">Married</label>
@@ -73,7 +130,7 @@ const PersonalInfo = () => {
           <h5>PAN Number</h5>
           <div className="shortforms-container">
             <div className="form__group field">
-              <input value="" className="form__field" type="text" id="l-pan" placeholder="PAN Number" required="" />
+              <input value={panNumber} className="form__field" type="text" id="l-pan" placeholder="PAN Number" required="" onChange={(e) => setPanNumber(e.target.value)} />
               <label className="form__label" for="l-pan">
                 PAN Number
               </label>
@@ -81,7 +138,7 @@ const PersonalInfo = () => {
           </div>
 
           <div className="save-options">
-            <button type="button" className="save-personal" id="save-personal" onClick={() => setEditing(false)}>
+            <button type="submit" className="save-personal" id="save-personal" onClick={() => setEditing(false)}>
               Save
             </button>
             <button type="button" className="cancel" id="cancel" onClick={() => setEditing(false)}>
@@ -93,31 +150,31 @@ const PersonalInfo = () => {
         <div className="before-edit">
           <div className="shortforms-container">
             <div className="form__group field">
-              <input readonly="" className="form__field" type="text" value="Venkitaraman Hariharan" id="full-name" placeholder="Full Name" required="" />
+              <input readonly className="form__field" type="text" value={`${firstName} ${lastName}`} id="full-name" placeholder="Full Name" required="" />
               <label className="form__label" for="full-name">
                 Full Name
               </label>
             </div>
             <div className="form__group field">
-              <input readonly="" className="form__field" type="text" value="01 / 11 / 2020" id="dob" placeholder="Date of Birth" required="" />
+              <input readonly className="form__field" type="text" value={'01 / 11 / 2020'} id="dob" placeholder="Date of Birth" required="" />
               <label className="form__label" for="dob">
                 Date of Birth
               </label>
             </div>
             <div className="form__group field">
-              <input readonly="" className="form__field" type="text" value="Male" id="gender" placeholder="Gender" required="" />
+              <input readonly className="form__field" type="text" value={gender} id="gender" placeholder="Gender" required="" />
               <label className="form__label" for="gender">
                 Gender
               </label>
             </div>
             <div className="form__group field">
-              <input readonly="" className="form__field" type="text" value="Single" id="marital-Status" placeholder="Marital Status" required="" />
+              <input readonly className="form__field" type="text" value={maritalStatus} id="marital-Status" placeholder="Marital Status" required="" />
               <label className="form__label" for="marital-Status">
                 Marital Status
               </label>
             </div>
             <div className="form__group field">
-              <input readonly="" className="form__field" type="text" value="XXXX728KYZ1" id="pan-num" placeholder="PAN Number" required="" />
+              <input readonly className="form__field" type="text" value={panNumber} id="pan-num" placeholder="PAN Number" required="" />
               <label className="form__label" for="pan-num">
                 PAN Number
               </label>
