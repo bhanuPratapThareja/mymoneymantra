88fe4772c8d05949dd262cf898a94bd8a4aa9bea
ChandrakantPal <sanjay161097@gmail.com>
2021-02-10 01:31:17 +0530

    bankname and logo displayed from fetched banks data

--- components/CreditScore/TotalActiveAccounts.js
+++ components/CreditScore/TotalActiveAccounts.js
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import moment from 'moment'
+import { findBank } from '../../utils/findBank'
 
-const TotalActiveAccounts = ({ active, closed, name }) => {
+const TotalActiveAccounts = ({ active, closed, name, banks }) => {
   const [activeTab, setActiveTab] = useState('accepted')
 
   return (
@@ -13,14 +14,18 @@ const TotalActiveAccounts = ({ active, closed, name }) => {
         <div className="container head">
           <div className="switch-tab tab">
             <button
-              className={activeTab === 'accepted' ? 'tablinks active' : 'tablinks'}
+              className={
+                activeTab === 'accepted' ? 'tablinks active' : 'tablinks'
+              }
               onClick={() => setActiveTab('accepted')}
             >
               <span></span>
               <h2>Active</h2>
             </button>
             <button
-              className={activeTab === 'rejected' ? 'tablinks active' : 'tablinks'}
+              className={
+                activeTab === 'rejected' ? 'tablinks active' : 'tablinks'
+              }
               onClick={() => setActiveTab('rejected')}
             >
               <span></span>
@@ -35,48 +40,53 @@ const TotalActiveAccounts = ({ active, closed, name }) => {
             className="cards-wrapper container tabcontent"
             style={{ display: 'grid' }}
           >
-            {active?.map((item, i) => (
-              <div key={i} className="popular-cards-slider-card">
-                <div className="popular-cards-slider-card-top">
-                  <div className="head">
-                    <h3>
-                      <b className="card_name">Citi Bank</b>
-                      <br />
-                      Platinum Delight Credit Card
-                    </h3>
-                    <img src="https://the1thing.github.io/MyMoneyMantra/build/images/icons/citi-logo.png" />
-                  </div>
-                  <div className="account-number">
-                    <p>{item.accountNo}</p>
-                  </div>
-                  <div className="app_progress_card_content">
-                    <div className="left">
-                      <div className="value">
-                        <span>Issued On:</span>
-                        <h5>{moment(item.issuedOn).format('DD MMM YYYY')}</h5>
-                      </div>
-                      <div className="value">
-                        <span>Last Payment Date:</span>
-                        <h5>₹{item.lastPaymentDate}</h5>
-                      </div>
-                      <div className="value">
-                        <span>Current Balance:</span>
-                        <h5>₹{item.currentBalance}</h5>
-                      </div>
-                      <div className="value">
-                        <span>Credit Limit:</span>
-                        <h5>₹{item.creditLimit}</h5>
+            {active?.map((item, i) => {
+              const bank = findBank(banks, item.bankId)
+              return (
+                <div key={i} className="popular-cards-slider-card">
+                  <div className="popular-cards-slider-card-top">
+                    <div className="head">
+                      <h3>
+                        <b className="card_name">{bank?.bank_name}</b>
+                        <br />
+                        Platinum Delight Credit Card
+                      </h3>
+                      <img
+                        src={`http://203.122.46.189:1338${bank?.bank_logo?.url}`}
+                      />
+                    </div>
+                    <div className="account-number">
+                      <p>{item.accountNo}</p>
+                    </div>
+                    <div className="app_progress_card_content">
+                      <div className="left">
+                        <div className="value">
+                          <span>Issued On:</span>
+                          <h5>{moment(item.issuedOn).format('DD MMM YYYY')}</h5>
+                        </div>
+                        <div className="value">
+                          <span>Last Payment Date:</span>
+                          <h5>₹{item.lastPaymentDate}</h5>
+                        </div>
+                        <div className="value">
+                          <span>Current Balance:</span>
+                          <h5>₹{item.currentBalance}</h5>
+                        </div>
+                        <div className="value">
+                          <span>Credit Limit:</span>
+                          <h5>₹{item.creditLimit}</h5>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="popular-cards-slider-card-bottom">
-                  <div>
-                    <h5>{item.paymentStatus} Payment</h5>
+                  <div className="popular-cards-slider-card-bottom">
+                    <div>
+                      <h5>{item.paymentStatus} Payment</h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              )
+            })}
           </div>
         ) : (
           <div
@@ -84,37 +94,42 @@ const TotalActiveAccounts = ({ active, closed, name }) => {
             style={{ display: 'grid' }}
             className="cards-wrapper container tabcontent"
           >
-            {closed?.map((item, i) => (
-              <div key={i} className="popular-cards-slider-card">
-                <div className="popular-cards-slider-card-top">
-                  <div className="head">
-                    <h3>
-                      <b className="card_name">Citi Bank</b>
-                      <br />
-                      Platinum Delight Credit Card
-                    </h3>
-                    <img src="https://the1thing.github.io/MyMoneyMantra/build/images/icons/citi-logo.png" />
-                  </div>
-                  <div className="app_progress_card_content">
-                    <div className="left">
-                      <div className="value">
-                        <span>Applicant’s Name:</span>
-                        <h5>{name}</h5>
-                      </div>
-                      <div className="value">
-                        <span>Product Type:</span>
-                        <h5>{item.productType}</h5>
+            {closed?.map((item, i) => {
+              const bank = findBank(banks, item.bankId)
+              return (
+                <div key={i} className="popular-cards-slider-card">
+                  <div className="popular-cards-slider-card-top">
+                    <div className="head">
+                      <h3>
+                        <b className="card_name">{bank?.bank_name}</b>
+                        <br />
+                        Platinum Delight Credit Card
+                      </h3>
+                      <img
+                        src={`http://203.122.46.189:1338${bank?.bank_logo?.url}`}
+                      />
+                    </div>
+                    <div className="app_progress_card_content">
+                      <div className="left">
+                        <div className="value">
+                          <span>Applicant’s Name:</span>
+                          <h5>{name}</h5>
+                        </div>
+                        <div className="value">
+                          <span>Product Type:</span>
+                          <h5>{item.productType}</h5>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="popular-cards-slider-card-bottom rejected-red">
-                  <div>
-                    <h5>Rejected: Due to poor Credit Score</h5>
+                  <div className="popular-cards-slider-card-bottom rejected-red">
+                    <div>
+                      <h5>Rejected: Due to poor Credit Score</h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              )
+            })}
           </div>
         )}
       </div>
