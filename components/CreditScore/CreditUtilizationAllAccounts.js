a53c1ec06934e3ff4e96f0783ed035d989db1b02
ChandrakantPal <sanjay161097@gmail.com>
2021-02-09 17:34:14 +0530

    key issue fixed

--- components/CreditScore/CreditUtilizationAllAccounts.js
+++ components/CreditScore/CreditUtilizationAllAccounts.js
@@ -11,11 +11,17 @@ const CreditUtilizationAllAccounts = ({ active, closed, name }) => {
       <div className="product-wrapper">
         <div className="container head">
           <div className="switch-tab tab">
-            <button className={activeTab === 'accepted' ? 'tablinks active' : 'tablinks'} onClick={() => setActiveTab('accepted')}>
+            <button
+              className={activeTab === 'accepted' ? 'tablinks active' : 'tablinks'}
+              onClick={() => setActiveTab('accepted')}
+            >
               <span></span>
               <h2>Active</h2>
             </button>
-            <button className={activeTab === 'rejected' ? 'tablinks active' : 'tablinks'} onClick={() => setActiveTab('rejected')}>
+            <button
+              className={activeTab === 'rejected' ? 'tablinks active' : 'tablinks'}
+              onClick={() => setActiveTab('rejected')}
+            >
               <span></span>
               <h2>Closed</h2>
             </button>
@@ -25,7 +31,7 @@ const CreditUtilizationAllAccounts = ({ active, closed, name }) => {
         {activeTab === 'accepted' ? (
           <div id="accepted" className="cards-wrapper container tabcontent">
             {active?.map((item, i) => (
-              <div className="popular-cards-slider-card">
+              <div key={i} className="popular-cards-slider-card">
                 <div key={i} className="popular-cards-slider-card-top">
                   <div className="head">
                     <h3>
@@ -56,13 +62,22 @@ const CreditUtilizationAllAccounts = ({ active, closed, name }) => {
                         <div className="content-section-right-percentage">
                           <div id="cu_percent">
                             <svg viewBox="0 0 100 100" style={{ display: 'block', width: '100%' }}>
-                              <path d="M 50,50 m 0,-45 a 45,45 0 1 1 0,90 a 45,45 0 1 1 0,-90" stroke="#eee" stroke-width="10" fill-opacity="0"></path>
+                              <path
+                                d="M 50,50 m 0,-45 a 45,45 0 1 1 0,90 a 45,45 0 1 1 0,-90"
+                                stroke="#eee"
+                                strokeWidth="10"
+                                fillOpacity="0"
+                              ></path>
                               <path
                                 d="M 50,50 m 0,-45 a 45,45 0 1 1 0,90 a 45,45 0 1 1 0,-90"
                                 stroke="rgb(137,193,66)"
-                                stroke-width="10"
-                                fill-opacity="0"
-                                style={{ strokeDasharray: '282.783, 282.783', strokeDashoffset: '56.5566', strokeLinecap: 'round' }}
+                                strokeWidth="10"
+                                fillOpacity="0"
+                                style={{
+                                  strokeDasharray: '282.783, 282.783',
+                                  strokeDashoffset: '56.5566',
+                                  strokeLinecap: 'round',
+                                }}
                               ></path>
                             </svg>
                             <div
@@ -97,7 +112,11 @@ const CreditUtilizationAllAccounts = ({ active, closed, name }) => {
             ))}
           </div>
         ) : (
-          <div id="rejected" className="cards-wrapper container tabcontent" style={{ display: 'grid' }}>
+          <div
+            id="rejected"
+            className="cards-wrapper container tabcontent"
+            style={{ display: 'grid' }}
+          >
             {closed?.map((item, i) => (
               <div key={i} className="popular-cards-slider-card">
                 <div className="popular-cards-slider-card-top">
