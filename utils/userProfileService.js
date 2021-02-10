8ba8d2b57242dc321ad173670038a1e19ac43c34
ChandrakantPal <sanjay161097@gmail.com>
2021-02-10 18:30:27 +0530

    getworkinfo added with a default customerId

--- utils/userProfileService.js
+++ utils/userProfileService.js
@@ -1,60 +1,64 @@
-import Axios from "axios"
-import moment from "moment";
-import { getApiData } from "../api/api"
+import Axios from 'axios'
+import moment from 'moment'
+import { getApiData } from '../api/api'
 
-export const getPersonalInfo=async ()=>{
-   try{
-       let customerId=await localStorage.getItem('customerId');
-       let {url}=getApiData('getPersonalInfo');
-   let response  = await Axios.get(url,{params:{customerId}});
-   return response.data
-   }
-   catch(err){
-       throw err.response.data
-   }
+export const getPersonalInfo = async () => {
+  try {
+    let customerId = await localStorage.getItem('customerId')
+    let { url } = getApiData('getPersonalInfo')
+    let response = await Axios.get(url, { params: { customerId } })
+    return response.data
+  } catch (err) {
+    throw err.response.data
+  }
 }
 
-export const getWorkInfo=async()=>{
-    
-    try{
-        let customerId=await localStorage.getItem('customerId');
-        let {url}=getApiData('workProfile');
-    let response  = await Axios.get(url,{params:{customerId}});
+export const getWorkInfo = async () => {
+  try {
+    let customerId = await localStorage.getItem('customerId')
+    let { url } = getApiData('workProfile')
+    let response = await Axios.get(
+      `${url}?customerId=${customerId ? customerId : 206}`
+    )
     return response.data
-    }
-    catch(err){
-        throw err.response.data
-    }
+  } catch (err) {
+    throw err.response.data
+  }
 }
 
-export const savePersonalInfo=async (name,lastname,gender,martialStatus,panNo,dob)=>{
-    try{
-        let customerId=await localStorage.getItem('customerId');
-        const  {url,body}=getApiData('savePersonalInfo');
-        body.customerId=customerId;
-        body.firstName=name?name:null;
-        body.lastName=lastname?lastname:null;
-        body.gender=gender?gender:null;
-        body.martialStatus=martialStatus?martialStatus:null;
-        body.panNo=panNo?panNo:null
-        body.dob=dob?moment(dob,'YYYY-MM-DD').format('DD/MM/YYYY'):null;
-    let response  = await Axios.post(url,body);
+export const savePersonalInfo = async (
+  name,
+  lastname,
+  gender,
+  martialStatus,
+  panNo,
+  dob
+) => {
+  try {
+    let customerId = await localStorage.getItem('customerId')
+    const { url, body } = getApiData('savePersonalInfo')
+    body.customerId = customerId
+    body.firstName = name ? name : null
+    body.lastName = lastname ? lastname : null
+    body.gender = gender ? gender : null
+    body.martialStatus = martialStatus ? martialStatus : null
+    body.panNo = panNo ? panNo : null
+    body.dob = dob ? moment(dob, 'YYYY-MM-DD').format('DD/MM/YYYY') : null
+    let response = await Axios.post(url, body)
     return response.data
-    }
-    catch(err){
-        throw err.response.data
-    }
- }
+  } catch (err) {
+    throw err.response.data
+  }
+}
 
- export const getContactInfo = async ()=>{
-     try{
-        let customerId=await localStorage.getItem('customerId');
-        const  {url}=getApiData('contactProfile');
-        let response  = await Axios.get(url,{params:{customerId}});
-        return response.data;
-     }
-     catch(err){
-         console.log(err);
-         throw(err);
-     }
- }
+export const getContactInfo = async () => {
+  try {
+    let customerId = await localStorage.getItem('customerId')
+    const { url } = getApiData('contactProfile')
+    let response = await Axios.get(url, { params: { customerId } })
+    return response.data
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
