472c4679186ff476b2d25f6f83d54ed2e4d324b5
ChandrakantPal <sanjay161097@gmail.com>
2021-02-10 18:33:29 +0530

    query companies with company names and handling company change handeling with other change handlers with getwork papulationg the feild

--- components/UserProfile/WorkInfo.js
+++ components/UserProfile/WorkInfo.js
@@ -1,23 +1,49 @@
-import { useState, useEffect } from "react";
-import { getWorkInfo } from "../../utils/userProfileService";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import { getWorkInfo } from '../../utils/userProfileService'
+import { getApiData } from '../../api/api'
 
 const WorkInfo = (props) => {
-  const [isedit, setisedit] = useState(false);
-  const [employedType, setemployedType] = useState(null);
-  const [companyId, setcompanyId] = useState(null);
-  const [bankId, setbankId] = useState(null);
-  const [ifscCode, setifscCode] = useState(null);
-  const [netMonthlyIncome, setnetMonthlyIncome] = useState(null);
-  const [accountNo, setaccountNo] = useState(null);
-  const [companyOptions, setcompanyOtions] = useState([])
+  const [isedit, setIsedit] = useState(false)
+  const [employedType, setEmployedType] = useState(null)
+  const [companyQuery, setCompanyQuery] = useState('')
+  const [bankId, setBankId] = useState(null)
+  const [ifscCode, setIfscCode] = useState(null)
+  const [netMonthlyIncome, setNetMonthlyIncome] = useState(null)
+  const [accountNo, setAccountNo] = useState(null)
+  const [companyOptions, setCompanyOtions] = useState([])
+  const [companyId, setCompanyId] = useState('')
+  const [companyName, setCompanyName] = useState('')
+
   useEffect(() => {
-    console.log(props.data);
-  }, []);
+    console.log(props.data)
+    getWork()
+  }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      if (companyQuery.length > 0) {
+        try {
+          const { url, body } = getApiData('company')
+          body.request.payload.name = companyQuery
+          const responseObject = await axios.post(url, body)
+          const { data } = responseObject
+          console.log({ data })
+          setCompanyOtions(data?.response?.payload?.companyMaster)
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [companyQuery])
 
   const getWork = () => {
     getWorkInfo()
       .then((res) => {
-        console.log(res);
+        console.log({ res })
         const {
           employedType,
           companyId,
@@ -25,29 +51,48 @@ const WorkInfo = (props) => {
           bankId,
           accountNo,
           ifscCode,
-        } = { ...res };
-        setcompanyId(companyId);
-        setemployedType(employedType);
-        setnetMonthlyIncome(netMonthlyIncome);
-        setbankId(bankId);
-        setaccountNo(accountNo);
-        setifscCode(ifscCode);
+          companyName,
+        } = { ...res }
+        setCompanyId(companyId)
+        setEmployedType(employedType)
+        setNetMonthlyIncome(netMonthlyIncome)
+        setBankId(bankId)
+        setAccountNo(accountNo)
+        setIfscCode(ifscCode)
+        setCompanyName(companyName)
+        setCompanyQuery(companyName)
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err)
       })
-      .finally(() => {});
-  };
+      .finally(() => {})
+  }
 
-  
+  const submitHandler = (e) => {
+    e.preventDefault()
+    setIsedit(false)
+  }
+
+  const selectCompanyId = (id, name) => {
+    setCompanyId(id)
+    setCompanyName(name)
+    setCompanyOtions([])
+    setCompanyQuery('')
+  }
+
+  const companyNameChangeHandler = (e) => {
+    setCompanyName(e.target.value)
+    setCompanyQuery(e.target.value)
+  }
+
   return (
-    <form className="work-wrapper">
+    <form className="work-wrapper" onSubmit={submitHandler}>
       <div className="shortforms-container">
         <div
           className={
             isedit
-              ? "form__group field edit-part"
-              : "form__group field read-part"
+              ? 'form__group field edit-part'
+              : 'form__group field read-part'
           }
         >
           <select
@@ -58,11 +103,12 @@ const WorkInfo = (props) => {
             id="emp-type"
             placeholder="Employment Type"
             required=""
+            onChange={(e) => setEmployedType(e.target.value)}
           >
-            <option val={1000000001}>Self Employed</option>
-            <option val={1000000002}>Self Employed Professional</option>
-            <option val={1000000004}>Salaried</option>
-            <option val={1000000008}>Defense</option>
+            <option value={1000000001}>Self Employed</option>
+            <option value={1000000002}>Self Employed Professional</option>
+            <option value={1000000004}>Salaried</option>
+            <option value={1000000008}>Defense</option>
             <label className="form__label" htmlFor="emp-type">
               Employment Type
             </label>
@@ -71,31 +117,50 @@ const WorkInfo = (props) => {
         <div
           className={
             isedit
-              ? "form__group field edit-part"
-              : "form__group field read-part"
+              ? 'form__group field edit-part'
+              : 'form__group field read-part'
           }
         >
           <input
             readOnly={!isedit}
             className="form__field"
             type="text"
-            value={companyId}
+            value={companyName}
             id="company-name"
             placeholder="Company Name"
             required=""
+            onChange={companyNameChangeHandler}
           />
           <label className="form__label" htmlFor="company-name">
             Company Name
           </label>
-          <datalist id="company-name">
-            {companyOptions.map((item)=><option key={item.caseCompanyId} value={item.caseCompanyId}>{item.companyName}</option>)}
-          </datalist>
+          {isedit && companyOptions.length > 0 && companyQuery.length > 0 && (
+            <datalist
+              id="company-name"
+              style={{ display: 'block', background: '#fff' }}
+            >
+              {isedit &&
+                companyOptions.length > 0 &&
+                companyQuery.length > 0 &&
+                companyOptions.map((item) => (
+                  <option
+                    key={item.caseCompanyId}
+                    value={item.caseCompanyId}
+                    onClick={() =>
+                      selectCompanyId(item.caseCompanyId, item.companyName)
+                    }
+                  >
+                    {item.companyName}
+                  </option>
+                ))}
+            </datalist>
+          )}
         </div>
         <div
           className={
             isedit
-              ? "form__group field edit-part"
-              : "form__group field read-part"
+              ? 'form__group field edit-part'
+              : 'form__group field read-part'
           }
         >
           <input
@@ -106,6 +171,7 @@ const WorkInfo = (props) => {
             id="monthly-income"
             placeholder="Net Monthly Income"
             required=""
+            onChange={(e) => setNetMonthlyIncome(e.target.value)}
           />
           <label className="form__label" htmlFor="monthly-income">
             Net Monthly Income
@@ -114,8 +180,8 @@ const WorkInfo = (props) => {
         <div
           className={
             isedit
-              ? "form__group field edit-part"
-              : "form__group field read-part"
+              ? 'form__group field edit-part'
+              : 'form__group field read-part'
           }
         >
           <select
@@ -126,12 +192,13 @@ const WorkInfo = (props) => {
             id="bank-name"
             placeholder="Bank Name"
             required=""
+            onChange={(e) => setBankId(e.target.value)}
           >
             <label className="form__label" htmlFor="bank-name">
               Bank Name
             </label>
             {props.data.map((item) => (
-              <option key={item.bank_id} val={item.bank_id}>
+              <option key={item.bank_id} value={item.bank_id}>
                 {item.bank_name}
               </option>
             ))}
@@ -140,8 +207,8 @@ const WorkInfo = (props) => {
         <div
           className={
             isedit
-              ? "form__group field edit-part"
-              : "form__group field read-part"
+              ? 'form__group field edit-part'
+              : 'form__group field read-part'
           }
         >
           <input
@@ -152,6 +219,7 @@ const WorkInfo = (props) => {
             id="account-num"
             placeholder="Account Number"
             required=""
+            onChange={(e) => setAccountNo(e.target.value)}
           />
           <label className="form__label" htmlFor="account-num">
             Account Number
@@ -160,8 +228,8 @@ const WorkInfo = (props) => {
         <div
           className={
             isedit
-              ? "form__group field edit-part"
-              : "form__group field read-part"
+              ? 'form__group field edit-part'
+              : 'form__group field read-part'
           }
         >
           <input
@@ -172,6 +240,7 @@ const WorkInfo = (props) => {
             id="ifsc"
             placeholder="IFSC Code"
             required=""
+            onChange={(e) => setIfscCode(e.target.value)}
           />
           <label className="form__label" htmlFor="ifsc">
             IFSC Code
@@ -179,14 +248,14 @@ const WorkInfo = (props) => {
         </div>
       </div>
       <div className="save-options">
-        <button type="button" className="save-work" id="save-work">
+        <button type="submit" className="save-work" id="save-work">
           Save
         </button>
         <button
           type="button"
           className="cancel"
           id="cancel"
-          onClick={() => setisedit(!isedit)}
+          onClick={() => setIsedit(!isedit)}
         >
           Cancel
         </button>
@@ -196,12 +265,12 @@ const WorkInfo = (props) => {
         type="button"
         id="edit-work"
         className="edit-button"
-        onClick={() => setisedit(!isedit)}
+        onClick={() => setIsedit(!isedit)}
       >
         Edit
       </button>
     </form>
-  );
-};
+  )
+}
 
-export default WorkInfo;
+export default WorkInfo
