776c7bc44d7d26cb315b5828d3b189473338b9dc
ChandrakantPal <sanjay161097@gmail.com>
2021-02-10 18:31:02 +0530

    query company with company name

--- components/UserProfile/ContactInfo.js
+++ components/UserProfile/ContactInfo.js
@@ -1,114 +1,152 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import {getContactInfo} from "../../utils/userProfileService";
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { getContactInfo } from '../../utils/userProfileService'
 const ContactInfo = () => {
-  const [editing, setEditing] = useState(false);
-  const [mobile, setmobile] = useState("");
-  const [email, setemail] = useState("");
-  const [currentAddress, setcurrentAddress] = useState("");
-  const [permanentAddress, setpermanentAddress] = useState("");
+  const [editing, setEditing] = useState(false)
+  const [mobile, setMobile] = useState('')
+  const [email, setEmail] = useState('')
+  const [currentAddress, setCurrentAddress] = useState('')
+  const [permanentAddress, setPermanentAddress] = useState('')
   useEffect(() => {
-    getContactInfo().then(res=>{
-      console.log(res);
-      const {customerId,emailId,mobileNo,address}=res;
-      setemail(emailId);
-      setmobile(mobileNo);
-    }).catch(err=>{
-      console.log(err);
-    })
+    getContactInfo()
+      .then((res) => {
+        console.log(res)
+        const { customerId, emailId, mobileNo, address } = res
+        setemail(emailId)
+        setmobile(mobileNo)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }, [])
+  const submitHandler = (e) => {
+    e.preventDefault()
+    setEditing(false)
+  }
   return (
     <>
-        <form className="contact-wrapper" style={{ display: "block" }}>
-          <div className="shortforms-container">
-            <div className={editing?'form__group field edit-part':'form__group field read-part'}>
-              <input readOnly={!editing}
-                className="form__field"
-                type="text"
-                value={mobile}
-                id="mob-num"
-
-                placeholder="Mobile Number"
-                required=""
-              />
-              <label className="form__label" htmlFor="mob-num">
-                Mobile Number
-              </label>
-            </div>
-            <div className={editing?'form__group field edit-part':'form__group field read-part'}>
-              <input readOnly={!editing}
-                className="form__field"
-                type="text"
-                value={email}
-                id="email"
-                placeholder="Email ID"
-                required=""
-              />
-              <label className="form__label" htmlFor="email">
-                Email ID
-              </label>
-            </div>
-            <div className={editing?'form__group field edit-part':'form__group field read-part'}>
-              <input readOnly={!editing}
-                className="form__field"
-                type="text"
-                value={currentAddress}
-                id="current-address"
-                placeholder="Current Address"
-                required=""
-              />
-              <label className="form__label" htmlFor="current-address">
-                Current Address
-              </label>
-            </div>
-            <div className={editing?'form__group field edit-part':'form__group field read-part'}>
-              <input readOnly={!editing}
-                className="form__field"
-                type="text"
-                value={permanentAddress}
-                id="permanent-address"
-                placeholder="Permanent Address"
-                required=""
-              />
-              <label className="form__label" htmlFor="permanent-address">
-                Permanent Address
-              </label>
-            </div>
+      <form
+        className="contact-wrapper"
+        style={{ display: 'block' }}
+        onSubmit={submitHandler}
+      >
+        <div className="shortforms-container">
+          <div
+            className={
+              editing
+                ? 'form__group field edit-part'
+                : 'form__group field read-part'
+            }
+          >
+            <input
+              readOnly={!editing}
+              className="form__field"
+              type="text"
+              value={mobile}
+              id="mob-num"
+              onChange={(e) => setMobile(e.target.value)}
+              placeholder="Mobile Number"
+              required=""
+            />
+            <label className="form__label" htmlFor="mob-num">
+              Mobile Number
+            </label>
           </div>
-          <div className="save-options" style={{display:editing?"flex":"none"}}>
-            <button
-              type="button"
-              className="save-contact"
-              id="save-contact"
-              onClick={() => setEditing(false)}
-            >
-              Save
-            </button>
-            <button
-              type="button"
-              className="cancel"
-              id="cancel"
-              onClick={() => setEditing(false)}
-            >
-              Cancel
-            </button>
+          <div
+            className={
+              editing
+                ? 'form__group field edit-part'
+                : 'form__group field read-part'
+            }
+          >
+            <input
+              readOnly={!editing}
+              className="form__field"
+              type="text"
+              value={email}
+              id="email"
+              placeholder="Email ID"
+              onChange={(e) => setEmail(e.target.value)}
+              required=""
+            />
+            <label className="form__label" htmlFor="email">
+              Email ID
+            </label>
           </div>
-         {!editing ? (<button
+          <div
+            className={
+              editing
+                ? 'form__group field edit-part'
+                : 'form__group field read-part'
+            }
+          >
+            <input
+              readOnly={!editing}
+              className="form__field"
+              type="text"
+              value={currentAddress}
+              id="current-address"
+              placeholder="Current Address"
+              onChange={(e) => setCurrentAddress(e.target.value)}
+              required=""
+            />
+            <label className="form__label" htmlFor="current-address">
+              Current Address
+            </label>
+          </div>
+          <div
+            className={
+              editing
+                ? 'form__group field edit-part'
+                : 'form__group field read-part'
+            }
+          >
+            <input
+              readOnly={!editing}
+              className="form__field"
+              type="text"
+              value={permanentAddress}
+              id="permanent-address"
+              placeholder="Permanent Address"
+              onChange={(e) => setPermanentAddress(e.target.value)}
+              required=""
+            />
+            <label className="form__label" htmlFor="permanent-address">
+              Permanent Address
+            </label>
+          </div>
+        </div>
+        <div
+          className="save-options"
+          style={{ display: editing ? 'flex' : 'none' }}
+        >
+          <button type="submit" className="save-contact" id="save-contact">
+            Save
+          </button>
+          <button
+            type="button"
+            className="cancel"
+            id="cancel"
+            onClick={() => setEditing(false)}
+          >
+            Cancel
+          </button>
+        </div>
+        {!editing && (
+          <button
             type="button"
             id="edit-contact"
             className="edit-button"
             onClick={() => setEditing(true)}
           >
             Edit
-          </button>):<></>}
-          
-        </form>
-      
-          
-        
-          {/* read-part */}
-    </>
-  );
-};
+          </button>
+        )}
+      </form>
 
-export default ContactInfo;
+      {/* read-part */}
+    </>
+  )
+}
+
+export default ContactInfo
