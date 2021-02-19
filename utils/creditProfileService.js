ed725c09746cda287a07471a180a39db24cb8d82
ChandrakantPal <sanjay161097@gmail.com>
2021-02-19 17:28:10 +0530

    cp services

---
+++ utils/creditProfileService.js
@@ -0,0 +1,74 @@
+import Axios from 'axios'
+import { getApiData } from '../api/api'
+
+export const getCreditScore = async () => {
+  try {
+    const customerId = localStorage.getItem('customerId')
+    const { url, body } = getApiData('creditProfileScore')
+    body.customerId = customerId ? customerId : '2000006836'
+    const response = await Axios.post(`${url}`, body)
+    return response.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
+
+export const getCreditAccounts = async () => {
+  try {
+    let customerId = localStorage.getItem('customerId')
+    let { url, body } = getApiData('creditProfileAccounts')
+    body.customerId = customerId ? customerId : '2000006836'
+    let response = await Axios.post(`${url}`, body)
+    return response.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
+
+export const getCreditAge = async () => {
+  try {
+    let customerId = localStorage.getItem('customerId')
+    let { url, body } = getApiData('creditProfileAge')
+    body.customerId = customerId ? customerId : '2000006836'
+    let response = await Axios.post(`${url}`, body)
+    return response.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
+
+export const getCreditEnquiries = async () => {
+  try {
+    let customerId = localStorage.getItem('customerId')
+    let { url, body } = getApiData('creditProfileEnquiries')
+    body.customerId = customerId ? customerId : '2000006836'
+    let response = await Axios.post(`${url}`, body)
+    return response.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
+
+export const getCreditRank = async () => {
+  try {
+    let customerId = localStorage.getItem('customerId')
+    let { url, body } = getApiData('creditProfileRank')
+    body.customerId = customerId ? customerId : '2000006836'
+    let response = await Axios.post(`${url}`, body)
+    return response.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
+
+export const getCreditUtilization = async () => {
+  try {
+    let customerId = localStorage.getItem('customerId')
+    let { url, body } = getApiData('creditProfileUtilization')
+    body.customerId = customerId ? customerId : '2000006836'
+    let response = await Axios.post(`${url}`, body)
+    return response.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
