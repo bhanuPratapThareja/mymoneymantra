b49d5fad81b6097d8ac5fc4ef02ed041dd2adf96
ChandrakantPal <sanjay161097@gmail.com>
2021-02-23 17:49:02 +0530

    line chart

--- components/CreditScore/CreditScoreBanner.js
+++ components/CreditScore/CreditScoreBanner.js
@@ -1,4 +1,32 @@
-const CreditScoreBanner = () => {
+import { Line } from 'react-chartjs-2'
+
+const data = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+  datasets: [
+    {
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40],
+    },
+  ],
+}
+
+const CreditScoreBanner = ({}) => {
   return (
     <section
       data-aos="fade-up"
@@ -31,7 +59,27 @@ const CreditScoreBanner = () => {
             />
           </div>
           <div className="score-graph-img">
-            <h2>Graph</h2>
+            <Line
+              data={data}
+              width={478}
+              height={239}
+              options={{
+                maintainAspectRatio: false,
+                responsive: true,
+                scales: {
+                  yAxes: [
+                    {
+                      stacked: true,
+                    },
+                  ],
+                  xAxes: [
+                    {
+                      stacked: true,
+                    },
+                  ],
+                },
+              }}
+            />
           </div>
         </div>
       </div>
