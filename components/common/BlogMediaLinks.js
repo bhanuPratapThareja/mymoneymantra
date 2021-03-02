ceb4f544f14df3f1226c91ebeca487be71b7cba9
fssandhu93 <fateh.singh.sandhu93@gmail.com>
2021-03-02 19:39:11 +0530

    On Blog Add button on left side in blog mobile view, social icons will slide in and out on basis of click of that button. On Blog Cards overlap with social icons in mobile view.

--- components/common/BlogMediaLinks.js
+++ components/common/BlogMediaLinks.js
@@ -1,55 +1,120 @@
-import Strapi from "../../providers/strapi"
+import Strapi from "../../providers/strapi";
 
 import {
-    FacebookIcon,
-    FacebookShareButton,
-    FacebookShareCount,
-    LinkedinIcon,
-    LinkedinShareButton,
-    PinterestIcon,
-    PinterestShareButton,
-    TwitterIcon,
-    TwitterShareButton,
-    WhatsappShareButton,
-    WhatsappIcon,
-    EmailShareButton,
-    EmailIcon
+  FacebookIcon,
+  FacebookShareButton,
+  FacebookShareCount,
+  LinkedinIcon,
+  LinkedinShareButton,
+  PinterestIcon,
+  PinterestShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappShareButton,
+  WhatsappIcon,
+  EmailShareButton,
+  EmailIcon,
 } from "react-share";
+import { useState } from "react";
 
-const BlogMediaLinks = props => {
-    const strapi = new Strapi()
-    const actionAfterShare = (e) => {
-        console.log(e)
-    }
-    // const { blog_social_media_link_component } = props.data.blog_social_media_link
-    return (
-        <section id="blog-head">
-            <div id={props.blogShareButton ? 'pop_up_container' : 'blog-socials'}>
-                <div className={props.blogShareButton ? 'pop_up_share' : 'slide_cell share_buttons'}>
-                    <TwitterShareButton url={props.url} onShareWindowClose={() => props.openBlogShare(false)} >
-                        {props.blogShareButton ? <TwitterIcon size={32} /> : <TwitterIcon size={36} />}
-                    </TwitterShareButton>
-                    <FacebookShareButton onShareWindowClose={() => props.openBlogShare(false)} url={props.url}>
-                        {props.blogShareButton ? <FacebookIcon size={32} /> : <FacebookIcon size={36} />}
-                    </FacebookShareButton>
-                    {props.blogData.image ? <PinterestShareButton media={`${props.blogData.image.url ? props.blogData.image.url : '/assets/images/icons/twitter.svg'}`} url={props.url} onShareWindowClose={() => props.openBlogShare(false)}>
-                        {props.blogShareButton ? <PinterestIcon size={32} /> : <PinterestIcon size={36} />}
-                    </PinterestShareButton> : null}
-                    <LinkedinShareButton url={props.url} onShareWindowClose={() => props.openBlogShare(false)} >
-                        {props.blogShareButton ? <LinkedinIcon size={32} /> : <LinkedinIcon size={36} />}
-                    </LinkedinShareButton>
-                     <WhatsappShareButton url={props.url} onShareWindowClose={() => props.openBlogShare(false)} >
-                        {props.blogShareButton ? <WhatsappIcon size={32} /> : <WhatsappIcon size={36} />}
-                    </WhatsappShareButton>
-                    <EmailShareButton url={props.url} onShareWindowClose={() => props.openBlogShare(false)} >
-                        {props.blogShareButton ? <EmailIcon size={32} /> : <EmailIcon size={36} />}
-                    </EmailShareButton>
-                    {props.blogShareButton ? <button onClick={() => props.openBlogShare(false)}>X</button> : null}
-                </div>
-            </div>
-        </section>
-    )
+const BlogMediaLinks = (props) => {
+  const [popTirgger, setPopTirgger] = useState(false);
+  const strapi = new Strapi();
+  const actionAfterShare = (e) => {
+    console.log(e);
+  };
+  // const { blog_social_media_link_component } = props.data.blog_social_media_link
+  return (
+    <section id="blog-head">
+      <div
+        id={props.blogShareButton ? "pop_up_container" : "blog-socials"}
+        className={popTirgger ? "show-social-icons" : " "}
+      >
+        <button
+          className="cstm-social-btn"
+          onClick={() => setPopTirgger(!popTirgger)}
+        >
+          <i></i>
+        </button>
+        <div
+          className={
+            props.blogShareButton ? "pop_up_share" : "slide_cell share_buttons"
+          }
+        >
+          <TwitterShareButton
+            url={props.url}
+            onShareWindowClose={() => props.openBlogShare(false)}
+          >
+            {props.blogShareButton ? (
+              <TwitterIcon size={32} />
+            ) : (
+              <TwitterIcon size={36} />
+            )}
+          </TwitterShareButton>
+          <FacebookShareButton
+            onShareWindowClose={() => props.openBlogShare(false)}
+            url={props.url}
+          >
+            {props.blogShareButton ? (
+              <FacebookIcon size={32} />
+            ) : (
+              <FacebookIcon size={36} />
+            )}
+          </FacebookShareButton>
+          {props.blogData.image ? (
+            <PinterestShareButton
+              media={`${
+                props.blogData.image.url
+                  ? props.blogData.image.url
+                  : "/assets/images/icons/twitter.svg"
+              }`}
+              url={props.url}
+              onShareWindowClose={() => props.openBlogShare(false)}
+            >
+              {props.blogShareButton ? (
+                <PinterestIcon size={32} />
+              ) : (
+                <PinterestIcon size={36} />
+              )}
+            </PinterestShareButton>
+          ) : null}
+          <LinkedinShareButton
+            url={props.url}
+            onShareWindowClose={() => props.openBlogShare(false)}
+          >
+            {props.blogShareButton ? (
+              <LinkedinIcon size={32} />
+            ) : (
+              <LinkedinIcon size={36} />
+            )}
+          </LinkedinShareButton>
+          <WhatsappShareButton
+            url={props.url}
+            onShareWindowClose={() => props.openBlogShare(false)}
+          >
+            {props.blogShareButton ? (
+              <WhatsappIcon size={32} />
+            ) : (
+              <WhatsappIcon size={36} />
+            )}
+          </WhatsappShareButton>
+          <EmailShareButton
+            url={props.url}
+            onShareWindowClose={() => props.openBlogShare(false)}
+          >
+            {props.blogShareButton ? (
+              <EmailIcon size={32} />
+            ) : (
+              <EmailIcon size={36} />
+            )}
+          </EmailShareButton>
+          {props.blogShareButton ? (
+            <button onClick={() => props.openBlogShare(false)}>X</button>
+          ) : null}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-}
-
-export default BlogMediaLinks
+export default BlogMediaLinks;
