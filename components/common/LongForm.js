c8573f6a2214fafc43e0deee803b2af9a9651abd
BHANU THAREJA <bhanuthareja@BHANUs-MacBook-Pro.local>
2021-03-04 00:11:15 +0530

    auto_select boolean field added for dropdowns

--- components/common/LongForm.js
+++ components/common/LongForm.js
@@ -218,7 +218,7 @@ class LongForm extends React.Component {
         updateDropdownList(inputs, listType, list, input_id)
         inputs.forEach(input => {
           if (input.input_id === input_id) {
-            if (list && list.length && field && field.value) {
+            if (list && list.length && field && field.value && input.auto_select) {
               let filteredItemList = list.filter(item => item[input.select_name] === field.value.toUpperCase())
               let filteredItem = filteredItemList.length ? filteredItemList[0] : null
               this.handleInputDropdownSelection(input_id, filteredItem)
